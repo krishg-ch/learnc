070434677682e6ac47fb6bd059743dee91aeeba5
Gopi Krishna <87570647+krishg-ch@users.noreply.github.com>
2024-07-10 23:08:19 +0530

    Create sample.c

---
+++ src/sample.c
@@ -0,0 +1,21 @@
+/* Documentation section
+   (About the code) */
+
+#include<stdio.h>        // Link section
+
+#define PI 3.14          // Definition section
+
+void add();              // Global declaration section
+int a = 2;
+
+int main()               // Main function
+{
+  // Body of the function
+  printf('This is a sample program');
+  return 0;              // return value from main function
+}
+
+void add()               // Subprogram section
+{
+  // Body of the subprogram
+}
